2cd34bc9298cb4f3b7f9c5adb25864f688c669d2
harshidha raja <harshidhav.raja@gmail.com>
2026-03-17 22:26:03 +0530

    uc3 done

--- OOPS.java
+++ OOPS.java
@@ -1,20 +1,3 @@
 public class OOPS{
-    public static void main(String[] args){
-        
-        System.out.println(
-                "  *****  " + "  *****  " + "  ****** " + "  ****** ");
-        System.out.println(
-                " *     * " + " *     * " + " *     * " + " *     * ");
-        System.out.println(
-                " *     * " + " *     * " + " *     * " + " *       " );
-        System.out.println(
-                " *     * " + " *     * " + "  ****** " + "  ****** ");
-        System.out.println(
-                " *     * " + " *     * " + " *       " + "       * ");
-        System.out.println(
-                " *     * " + " *     * " + " *       " + " *     * ");
-        System.out.println(
-                "  *****  " + "  *****  " + " *       " + "  ****** ");
-    }
-    
+    public static void main(String[] args){}
 }
